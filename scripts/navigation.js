757e19dc3798c45abc962cddcc2bd14c9ff032ec
danielndubuisi <ndubeansdaniel97@gmail.com>
2025-05-11 02:18:37 +0100

    updated design and layout

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,8 +1,9 @@
 // This file contains the navigation functionality for the website.
-const navLinks = document.querySelectorAll(".nav-links");
+const navLinks = document.querySelectorAll(".nav-link");
 const nav = document.querySelector("nav");
 const ul = document.querySelector("ul");
 const menuBtn = document.querySelector("#menu");
+const wayfinder = document.querySelector(".heading");
 
 // toggle menu on small screens
 menuBtn.addEventListener("click", () => {
@@ -12,7 +13,12 @@ menuBtn.addEventListener("click", () => {
 });
 
 [...navLinks].map((link) => {
+    // default wayfinder value
+    wayfinder.textContent = "Home";
+
     link.addEventListener("click", () => {
+        wayfinder.textContent = link.textContent;
+
         // close menu on small screens once link selected
         if (window.innerWidth < 659) {
             nav.classList.toggle("show");
