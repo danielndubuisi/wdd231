77278b2c4f3ff0e0ad93e49dc75635a7de79563e
danielndubuisi <ndubeansdaniel97@gmail.com>
2025-05-17 06:31:37 +0100

    nav and footer styles

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,3 +1,9 @@
+// This file contains the navigation functionality for the website.
+const navLinks = document.querySelectorAll(".nav-link");
+const nav = document.querySelector("nav");
+const ul = document.querySelector("ul");
+const menuBtn = document.querySelector("#menu");
+const wayfinder = document.querySelector(".heading");
 const cards = document.querySelector(".cards");
 
 const getMembersData = async () => {
@@ -11,12 +17,12 @@ const displayMembers = (members) => {
         // create card elements
         let card = document.createElement("div");
         let heading = document.createElement("h3");
-        let subHeading = document.createElement("small")
-        let line = document.createElement("hr")
+        let subHeading = document.createElement("small");
+        let line = document.createElement("hr");
         let email = document.createElement("p");
         let phone = document.createElement("p");
         let url = document.createElement("p");
-        
+
         // provide values
         heading.textContent = `${member.name}`;
         subHeading.textContent = `${member.tagLine}`;
@@ -39,3 +45,46 @@ const displayMembers = (members) => {
 
 // fetch and display members
 getMembersData();
+
+// set wayfinder and activelink based on current page
+const setActiveLink = () => {
+    const currentPage = window.location.pathname.split("/").pop();
+    // if currentPage is directory, set it to index.html
+    const activeLink = currentPage === "directory.html" ? "Home" : currentPage;
+
+    navLinks.forEach((link) => {
+        const linkPage = link.getAttribute("href");
+        if (activeLink === linkPage) {
+            link.classList.add("active");
+            wayfinder.textContent = link.textContent;
+        } else if (activeLink === "Home") {
+            wayfinder.textContent = "Home";
+        } else {
+            link.classList.remove("active");
+        }
+    });
+};
+
+// initialize active link and wayfinder on page load
+setActiveLink();
+
+// toggle menu on small screens
+menuBtn.addEventListener("click", () => {
+    nav.classList.toggle("show");
+    ul.classList.toggle("show");
+    menuBtn.classList.toggle("show");
+});
+
+// update wayfinder on link click
+[...navLinks].map((link) => {
+    link.addEventListener("click", () => {
+        wayfinder.textContent = link.textContent;
+
+        // close menu on small screens once link selected
+        if (window.innerWidth < 659) {
+            nav.classList.toggle("show");
+            ul.classList.toggle("show");
+            menuBtn.classList.toggle("show");
+        }
+    });
+});
